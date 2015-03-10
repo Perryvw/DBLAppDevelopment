77e84a932f5d6a0352293abebdd57401a97331e1
NBraat <nickbraat@planet.nl>
2015-03-10 14:23:21 +0100

    Activity_Birth working

--- Hitch/app/src/main/java/com/dblappdev/hitch/app/BirthActivity.java
+++ Hitch/app/src/main/java/com/dblappdev/hitch/app/BirthActivity.java
@@ -18,7 +18,7 @@ public class BirthActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_birth);
     }
 
     /**
