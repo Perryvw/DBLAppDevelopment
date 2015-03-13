a2fe0cbccc6bd38cab336ddc52c573987b779fe9
ziyed92 <ziadbensnaiba@gmail.com>
2015-03-13 14:04:55 +0100

    Play services
    Google play services with a test map in the mainactivity (previous code
    is commented out)

--- Hitch/app/src/main/java/com/dblappdev/hitch/app/MainActivity.java
+++ Hitch/app/src/main/java/com/dblappdev/hitch/app/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 
 
 public class MainActivity extends Activity {
@@ -14,6 +16,7 @@ public class MainActivity extends Activity {
     public static final String SHARED_PREF = "com.dblappdev.hitch";
     public static final String BIRTH_KEY = "com.dblappdev.hitch.birth";
     public static final String DRIVER_MODE_KEY = "com.dblappdev.hitch.mode";
+    private GoogleApiClient mGoogleApiClient;
 
     /**
      * When the app is giving birth the intent BirthActivity will be started. Otherwise we will start TabViewActivity.
@@ -22,16 +25,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences prefs = this.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
-        boolean birthControl = prefs.getBoolean(BIRTH_KEY, false);
-        if (! birthControl) {
-            Intent intent = new Intent(this, BirthActivity.class);
-            startActivity(intent);
-            return;
-        }
+        setContentView(R.layout.fragment_map);
 
-        Intent intent = new Intent(this, TabViewActivity.class);
-        startActivity(intent);
+//        Intent intent2 = new Intent(this, MapActivity.class);
+//        startActivity(intent2);
+//
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addApi(Drive.API)
+//                .addScope(Drive.SCOPE_FILE)
+//                .build();
+//        mGoogleApiClient.connect();
+//
+//        SharedPreferences prefs = this.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
+//        boolean birthControl = prefs.getBoolean(BIRTH_KEY, false);
+//        if (! birthControl) {
+//            Intent intent = new Intent(this, BirthActivity.class);
+//            startActivity(intent);
+//            return;
+//        }
+//
+//
+//
+//        Intent intent = new Intent(this, TabViewActivity.class);
+//        startActivity(intent);
     }
 
 
