0473327d47c9352a5d1379544acc1b63bcf93efb
ziyed92 <ziadbensnaiba@gmail.com>
2015-03-15 13:50:58 +0100

    Google MAPs
    Authentication is messing up at the moment

--- Hitch/app/src/main/java/com/dblappdev/hitch/app/MainActivity.java
+++ Hitch/app/src/main/java/com/dblappdev/hitch/app/MainActivity.java
@@ -14,30 +14,25 @@ public class MainActivity extends Activity {
     public static final String SHARED_PREF = "com.dblappdev.hitch";
     public static final String BIRTH_KEY = "com.dblappdev.hitch.birth";
     public static final String DRIVER_MODE_KEY = "com.dblappdev.hitch.mode";
+    public static final String STATE_KEY = "com.dblappdev.hitch.state";
 
     /**
      * When the app is giving birth the intent BirthActivity will be started. Otherwise we will start TabViewActivity.
      */
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        SharedPreferences prefs = this.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
-//        boolean birthControl = prefs.getBoolean(BIRTH_KEY, false);
-//        if (! birthControl) {
-//            Intent intent = new Intent(this, BirthActivity.class);
-//            startActivity(intent);
-//            return;
-//        }
-//
-//        Intent intent = new Intent(this, TabViewActivity.class);
-//        startActivity(intent);
-//    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_map);
+
+        SharedPreferences prefs = this.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
+        boolean birthControl = prefs.getBoolean(BIRTH_KEY, false);
+        if (! birthControl) {
+            Intent intent = new Intent(this, BirthActivity.class);
+            startActivity(intent);
+            return;
+        }
+
+        Intent intent = new Intent(this, TabViewActivity.class);
+        startActivity(intent);
     }
 
     @Override
