373fca134cff5d1544f503b39a26ae5749291bf1
uodasuodas <somiciuss@gmail.com>
2015-03-23 19:56:26 +0100

    Revert "UI"
    This reverts commit 4f375fe08888b0f2eaaecb2cabcdc38910f40357.

--- Hitch/app/src/main/java/com/dblappdev/hitch/app/EditProfileActivity.java
+++ Hitch/app/src/main/java/com/dblappdev/hitch/app/EditProfileActivity.java
@@ -1,16 +1,9 @@
 package com.dblappdev.hitch.app;
 
 import android.app.Activity;
-import android.os.Bundle;
 
 /**
  * Created by s128232 on 10-3-2015.
  */
 public class EditProfileActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_viewprofile);
-    }
 }
