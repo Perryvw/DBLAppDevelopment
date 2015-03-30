e3f56d87a07172db8e442817a5734fe01a44fc85
Perry <perry.wesel@gmail.com>
2015-03-30 11:25:53 +0200

    updated demo script

--- Backend/demoDB.php
+++ Backend/demoDB.php
@@ -17,10 +17,8 @@
 
 	$lastName = array("Smith", "Johnson", "Williams", "Brown", "Jones", "Davis", "Miller", "Garcia", "Rodriguez", "Wilson");
 
-	$locations = array("Amsterdam","Leeuwarden","Den Haag","Rotterdam","Breda","Utrecht","Eindhoven","Maastricht","Nijmegen","Groningen","Heerenveen", "Tilburg");
-
 	//How many sample users do we want to generate?
-	$USERS_TO_GENERATE = 3;
+	$USERS_TO_GENERATE = 100;
 	$HITCHHIKER_DISTRIBUTION = 0.2;
 
 	//Generate users
@@ -28,24 +26,39 @@
 		//Build a random name
 		$name = $firstName[rand(1, 16)]." ".$lastName[rand(0,9)];
 		
+		//not all users should be hitchhiking, probability based
+		$ishitching = rand(1,1000) < 1000 * $HITCHHIKER_DISTRIBUTION ? true : false;
+
 		//Insert into db and get user ID
-		$user_id = $db->insertRow('hitch_users', array(NULL, randomPhoneNumber(), rand(0,1), $name, date('Y-m-d', time() - 356.25*24*3600*rand(16,40)), date('Y-m-d'), 0, ''));
+		$user_id = $db->insertRow('hitch_users', array(NULL, randomPhoneNumber(), $ishitching ? 1 : 0, $name, date('Y-m-d', time() - 356.25*24*3600*rand(16,40)), date('Y-m-d'), 0, ''));
 
 		//generate hitchhike data
-		//not all users should be hitchhiking
-		$ishitching = rand(1,1000) < 1000 * $HITCHHIKER_DISTRIBUTION ? true : false;
 		$hitchhikedate = '1990-1-1';
 		if ($ishitching) {
 			$hitchhikedate = date('Y-m-d H:i:s', time() - rand(10, 3600*24*2));
 		}
 		$db->insertRow('hitch_hitchhikestatus', array($user_id, $hitchhikedate, randomLocation(), randomLocation()));
+
+		//generate some ratings for the user
+		$numRatings = rand(0, 5);
+		for ($j = 0; $j < $numRatings; $j++){
+			$db->insertRow('hitch_ratings', array($user_id, $user_id+$j+1, rand(0,5), ''));
+		}
+
+		//generate some routes for the user
+		$numRoutes = rand(1,5);
+		for ($j=0; $j < $numRoutes; $j++) {
+			$route_id = $db->insertRow('hitch_userroutes', array(null, $user_id, date('Y-m-d H:i:s',time() - rand(0, 3600*24*7)), randomLocation(), randomLocation()));
+		}
 	}
 
+	//Return a random location name
 	function randomLocation() {
-		echo $locations[rand(0,count($locations)-1)].'<br />';
+		$locations = array("Amsterdam","Leeuwarden","Den Haag","Rotterdam","Breda","Utrecht","Eindhoven","Maastricht","Nijmegen","Groningen","Heerenveen", "Tilburg");
 		return $locations[rand(0,count($locations)-1)];
 	}
 
+	//Return a random phone number
 	function randomPhoneNumber($length = 10) {
 		$nr = '';
 		for ($i=0; $i<$length; $i++) {
