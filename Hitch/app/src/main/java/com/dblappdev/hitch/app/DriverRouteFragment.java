82c6e233151b98d69917c2a323a3b2303ba70554
guusleijsten <g.h.p.leijsten@student.tue.nl>
2015-03-31 09:38:55 +0200

    Added fragment_driver_route plus some refactoring

--- Hitch/app/src/main/java/com/dblappdev/hitch/app/DriverRouteFragment.java
+++ Hitch/app/src/main/java/com/dblappdev/hitch/app/DriverRouteFragment.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 public class DriverRouteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_hitcher, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_driver_route, container, false);
 
         return rootView;
     }
