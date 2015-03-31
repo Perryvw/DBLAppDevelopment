14044bddf964e0562727fed301d773407128ada7
NBraat <nickbraat@planet.nl>
2015-03-31 18:43:45 +0200

    Chat bubbles!! :)

---
+++ Hitch/app/src/main/java/com/dblappdev/hitch/app/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.dblappdev.hitch.app;
+
+/**
+ * Created by s128232 on 31-3-2015.
+ */
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
