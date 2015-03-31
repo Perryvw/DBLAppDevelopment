97e9e964b0320ca733d473661ecec185d5abc5f9
guusleijsten <g.h.p.leijsten@student.tue.nl>
2015-03-31 11:18:21 +0200

    connecting chat to API (first shot)

--- Hitch/app/src/main/java/com/dblappdev/hitch/app/ChatActivity.java
+++ Hitch/app/src/main/java/com/dblappdev/hitch/app/ChatActivity.java
@@ -1,6 +1,8 @@
 package com.dblappdev.hitch.app;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.KeyEvent;
@@ -8,36 +10,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.dblappdev.hitch.model.User;
+import com.dblappdev.hitch.network.API;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Random;
+import java.util.concurrent.Callable;
 
 /**
  * Created by s128232 on 10-3-2015.
  */
 public class ChatActivity extends Activity {
 
+    //Preferences
+    private SharedPreferences prefs;
+
     private EditText messageText;
     private Button sendMessageButton;
     private EditText messageHistoryText;
 
-    //TESTING
-    private Random random;
     private String[][] chatMessages;
+
+    private final static int CHAT_ID = 1;
+    private final static int CHAT_LIMIT = 50;
+
+    private User sender;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_chat);
 
+        prefs = this.getSharedPreferences(MainActivity.SHARED_PREF, Context.MODE_PRIVATE);
+
         messageText = (EditText) findViewById(R.id.message);
         messageHistoryText = (EditText) findViewById(R.id.messageHistory);
         sendMessageButton = (Button) findViewById(R.id.sendMessageButton);
         messageText.requestFocus();
 
-
-
-        //TESTING
-        chatMessages = new String[25][2];
-        random = new Random();
         messageText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -57,9 +69,11 @@ public class ChatActivity extends Activity {
         sendMessageButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 sendMessage();
+                messageText.setText("");
             }
         });
 
+        /**
         //Fill array with fake chat data
         for (int i = 0; i<25; i++) {
             for (int j = 0; j<2; j++) {
@@ -76,12 +90,18 @@ public class ChatActivity extends Activity {
                 }
             }
         }
+         */
         loadMessageHistory();
     }
 
     public void sendMessage() {
         String message = messageText.getText().toString();
         appendToMessageHistory("You", message);
+        int userID = prefs.getInt(MainActivity.USER_KEY, -1);
+        if (userID == -1) {
+            return;
+        }
+        new API().insertChatMessage(userID, CHAT_ID, message, null);
     }
 
     public  void appendToMessageHistory(String username, String message) {
@@ -89,19 +109,36 @@ public class ChatActivity extends Activity {
             username = "Unknown";
             message = "Unknown";
         }
-            messageHistoryText.append(username + ":\n");
-            messageHistoryText.append(message + "\n");
+        messageHistoryText.append(username + ":\n");
+        messageHistoryText.append(message + "\n");
     }
 
     public void loadMessageHistory() {
-        for (int i = 0; i<chatMessages.length; i++) {
-            messageHistoryText.append(Html.fromHtml(chatMessages[i][0]) + ":\n");
-            messageHistoryText.append(Html.fromHtml(chatMessages[i][1]) + "\n");
-        }
+        final API api = new API();
+        api.getChatMessages(CHAT_ID, CHAT_LIMIT, "0", new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                loadMessageHistoryCallback(api);
+                return null;
+            }
+        });
     }
 
-    //TESTING
-    public boolean getRandomBoolean() {
-        return random.nextBoolean();
+    public void loadMessageHistoryCallback(API api) {
+        JSONObject json = api.getResponse();
+
+        try {
+            JSONArray arr = json.getJSONArray("messages");
+            for(int i = 0; i < arr.length(); i++) {
+                JSONObject message = arr.getJSONObject(i);
+
+                String sender = message.getString("username");
+                String text = message.getString("text");
+                appendToMessageHistory(sender, text);
+
+            }
+        } catch(JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
